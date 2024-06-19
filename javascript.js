c6a6be1bf2663eac9c7a83592a304139e06ed260
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-19 19:01:54 +0200

    feat: Add algorithm to get computer choice

--- javascript.js
+++ javascript.js
@@ -4,11 +4,20 @@ console.log("Starting game....");
 
 function getComputerChoice() {
     // INIT variable for random number
+    let randomOfThree;
     // COMPUTE random number from 1-3
     // SET computed number to variable
+    randomOfThree = Math.floor((Math.random() * 3)) + 1;
     // CALCULATE choice corresponding to random number
-        // IF number = 1 THEN choose ROCK
-        // ELSE choose PAPER
-        // ELSE choose SCISSORS
-    // RETURN the chosen value
+    if (randomOfThree === 1) {
+        return 'rock';
+    } else if (randomOfThree === 2) {
+        return 'paper'
+    } else if (randomOfThree === 3) {
+        return 'scissors'
+    } else {
+        console.log('No valid random number for this game!');
+    }
 }
+
+console.log('Computer choice is: '+ getComputerChoice());
