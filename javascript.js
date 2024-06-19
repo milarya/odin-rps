404fa95bba0eeac17411f77ecc9ac88f984a5514
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-19 18:49:28 +0200

    feat: Pseudocode for getComputerChoice algorithm

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+console.log("Starting game....");
+
+// calculate computer choice
+
+function getComputerChoice() {
+    // INIT variable for random number
+    // COMPUTE random number from 1-3
+    // SET computed number to variable
+    // CALCULATE choice corresponding to random number
+        // IF number = 1 THEN choose ROCK
+        // ELSE choose PAPER
+        // ELSE choose SCISSORS
+    // RETURN the chosen value
+}
